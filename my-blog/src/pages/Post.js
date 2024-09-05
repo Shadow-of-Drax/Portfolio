43523b21ba6705c9d7238f4f3966456b2edb0e85
James Warren <98415151+Shadow-of-Drax@users.noreply.github.com>
2024-09-05 10:58:05 -0400

    Updates Post.js and added CSS

--- my-blog/src/pages/Post.js
+++ my-blog/src/pages/Post.js
@@ -1,27 +1,24 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { useParams } from 'react-router-dom';
-import BlogPost from '../components/BlogPost';
+import './Post.css';
 
-const Post = () => {
+const Post = ({ posts }) => {
   const { id } = useParams();
-  const [post, setPost] = useState(null);
-
-  useEffect(() => {
-    // Mock fetch API
-    const fetchedPosts = [
-      { id: 1, title: 'My First Blog', author: 'James Warren', body: 'This is my first blog post...' },
-      { id: 2, title: 'From the beginning', author: 'James Warren', body: 'Started my careere in the Army in Febuary of 2003. Knowing exactyly what I was getting into since 9/11 was so recent. I enlisted into the Field Artillery as a Forward Observer. My first set of duties were as a driver of our vehical. I would learn the basics of my job and learn how to maintain and drive the vehical.' }
-    ];
-
-    const fetchedPost = fetchedPosts.find(p => p.id === parseInt(id));
-    setPost(fetchedPost);
-  }, [id]);
+  const post = posts.find((post) => post.id === parseInt(id));
 
   return (
-    <div className="post">
-      {post ? <BlogPost post={post} /> : <h2>Loading...</h2>}
+    <div className="post-details container">
+      {post ? (
+        <>
+          <h1>{post.title}</h1>
+          <p><strong>By {post.author}</strong></p>
+          <p>{post.body}</p>
+        </>
+      ) : (
+        <p>Post not found.</p>
+      )}
     </div>
   );
-}
+};
 
 export default Post;
