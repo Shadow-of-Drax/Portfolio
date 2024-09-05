586eebcf432143c5b91ab73650110531bc0a8413
James Warren <98415151+Shadow-of-Drax@users.noreply.github.com>
2024-09-05 09:42:47 -0400

    update

--- mini-netflix/src/components/Row.js
+++ mini-netflix/src/components/Row.js
@@ -5,13 +5,10 @@ import './row.css';
 function row({ title }) {
     const movies = [
         { id: 1, title: "Movie 1", imageUrl: "https://image.tmdb.org/t/p/original/movie-image1.jpg" },
-        { id: 2, name: "Movie 2", imageUrl: "https://image.tmdb.org/t/p/original/movie-image2.jpg" },
-        { id: 3, name: "Movie 3", imageUrl: "https://image.tmdb.org/t/p/original/movie-image3.jpg" },
+        { id: 2, title: "Movie 2", imageUrl: "https://image.tmdb.org/t/p/original/movie-image2.jpg" },
+        { id: 3, title: "Movie 3", imageUrl: "https://image.tmdb.org/t/p/original/movie-image3.jpg" },
         // Add more movies
     ];
-    { 1, name; "Movie 1", imageUrl; "https://image.tmdb.org/t/p/original/movie-image1.jpg" };
-    { 2, name; "Movie 2", imageUrl; "https://image.tmdb.org/t/p/original/movie-image2.jpg" };
-    { 3, name; "Movie 3", imageUrl; "https://image.tmdb.org/t/p/original/movie-image3.jpg" };
 
   return (
     <div className="row">
