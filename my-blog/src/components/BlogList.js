697bd559ba13f47d0bd798591c7454ea58d33fff
James Warren <98415151+Shadow-of-Drax@users.noreply.github.com>
2024-09-05 10:56:10 -0400

    Update BlogList.js

--- my-blog/src/components/BlogList.js
+++ my-blog/src/components/BlogList.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './BlogList.css';
 
 const BlogList = ({ posts, deletePost }) => {
   return (
     <div className="blog-list">
       {posts.map((post) => (
-        <div className="blog-preview" key={post.id}>
-          <Link to={`/post/${post.id}`}>
-            <h2>{post.title}</h2>
-            <p>Written by {post.author}</p>
-          </Link>
-          <Link to={`/edit/${post.id}`}>
-            <button>Edit</button>
-          </Link>
-          <button onClick={() => deletePost(post.id)}>Delete</button>
+        <div className="blog-card" key={post.id}>
+          <h2>{post.title}</h2>
+          <p>By {post.author}</p>
+          <p>{post.body.substring(0, 100)}...</p>
+          <div className="blog-actions">
+            <Link to={`/post/${post.id}`}>Read More</Link>
+            <Link to={`/edit/${post.id}`}>Edit</Link>
+            <button onClick={() => deletePost(post.id)}>Delete</button>
+          </div>
         </div>
       ))}
     </div>
