52514db355437c704a9ef5e9d148912d0b4b5c98
James Warren <98415151+Shadow-of-Drax@users.noreply.github.com>
2024-09-06 15:47:30 -0400

    Update Row.js

--- mini-netflix/src/components/row/Row.js
+++ mini-netflix/src/components/row/Row.js
@@ -5,7 +5,7 @@ import './row.css';
 
 function Row({ title }) {
     const movies = [
-        { id: 1, title: "Movie 1", imageUrl: "https://image.tmdb.org/t/p/original/movie-image1.jpg" },
+        { id: 1, title: "Movie 1", imageUrl: "https://c7.alamy.com/comp/K388N8/teenage-mutant-ninja-turtles-K388N8.jpg" },
         { id: 2, title: "Movie 2", imageUrl: "https://image.tmdb.org/t/p/original/movie-image2.jpg" },
         { id: 3, title: "Movie 3", imageUrl: "https://image.tmdb.org/t/p/original/movie-image3.jpg" },
         // Add more movies
