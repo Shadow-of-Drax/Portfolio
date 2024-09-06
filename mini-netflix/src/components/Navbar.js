008a362a2fc6c03e0e1323f256bf543e4dda9f7a
James Warren <98415151+Shadow-of-Drax@users.noreply.github.com>
2024-09-06 13:43:25 -0400

    Update Navbar.js

--- mini-netflix/src/components/Navbar.js
+++ mini-netflix/src/components/Navbar.js
@@ -19,8 +19,8 @@ function Navbar() {
       <div className="navbar">
         <img
           className="navbar__logo"
-          src="/assets/logo.png"
-          alt="Netflix Logo"
+          src="/assets/ninja.png"
+          alt="Ninja Logo"
         />
         <img
           className="navbar__avatar"
