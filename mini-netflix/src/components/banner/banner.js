89f958e7817015595e00e2ed410cd9fef6caf999
James Warren <98415151+Shadow-of-Drax@users.noreply.github.com>
2024-09-06 16:07:21 -0400

    Update banner.js

--- mini-netflix/src/components/banner/banner.js
+++ mini-netflix/src/components/banner/banner.js
@@ -2,18 +2,18 @@ import React from 'react';
 import './banner.css';
 
 function Banner() {
-  const backgroundImage = 'https://image.tmdb.org/t/p/original/your-image-path.jpg'; // Replace with an actual image URL
+  const backgroundImage = 'https://c7.alamy.com/comp/2K632KX/defocused-glow-overlay-neon-light-purple-color-ray-2K632KX.jpg'; // Replace with an actual image URL
 
   return (
     <header className="banner" style={{ backgroundImage: `url(${backgroundImage})` }}>
       <div className="banner__contents">
-        <h1 className="banner__title">Movie Title</h1>
+        <h1 className="banner__title">Back in the SaturDAY!</h1>
         <div className="banner__buttons">
           <button className="banner__button">Play</button>
           <button className="banner__button">My List</button>
         </div>
         <h1 className="banner__description">
-          This is a short description of the movie. It can be a couple of sentences that summarize the plot.
+          This is the beginning of a larger project.
         </h1>
       </div>
       <div className="banner--fadeBottom"></div>
