f95f5c74722cf5dbcbcd48a62bf0a298859803af
James Warren <98415151+Shadow-of-Drax@users.noreply.github.com>
2024-09-06 16:02:22 -0400

    Update Row.js

--- mini-netflix/src/components/row/Row.js
+++ mini-netflix/src/components/row/Row.js
@@ -6,8 +6,8 @@ import './row.css';
 function Row({ title }) {
     const movies = [
         { id: 1, title: "Movie 1", imageUrl: "https://c7.alamy.com/comp/K388N8/teenage-mutant-ninja-turtles-K388N8.jpg" },
-        { id: 2, title: "Movie 2", imageUrl: "https://image.tmdb.org/t/p/original/movie-image2.jpg" },
-        { id: 3, title: "Movie 3", imageUrl: "https://image.tmdb.org/t/p/original/movie-image3.jpg" },
+        { id: 2, title: "Movie 2", imageUrl: "https://c7.alamy.com/comp/H65YE0/die-echten-ghostbusters-aka-the-real-ghost-busters-tv-series-1986-H65YE0.jpg" },
+        { id: 3, title: "Movie 3", imageUrl: "https://c7.alamy.com/comp/2C3JNRF/beast-wars-transformers-sony-playstation-1-ps1-psx-editorial-use-only-2C3JNRF.jpg" },
         // Add more movies
     ];
 
