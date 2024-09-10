98145a3b79757d6b03bd453f5ec4a18dcc7c036f
James Warren <98415151+Shadow-of-Drax@users.noreply.github.com>
2024-09-10 15:40:42 -0400

    updates

--- mini-netflix/src/index.js
+++ mini-netflix/src/index.js
@@ -11,6 +11,8 @@ root.render(
   </React.StrictMode>
 );
 
+// mongodb+srv://jameswarren83:iMxqSdy4WIpnk38@james-warren-database.29bff.mongodb.net/?retryWrites=true&w=majority&appName=James-Warren-Database
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
