6d6bf5c4e79d577fbf9583e3b41332f3da5babc5
James Warren <98415151+Shadow-of-Drax@users.noreply.github.com>
2024-09-09 21:47:00 -0400

    Update npcData.js

---
+++ npc-generator/src/Data/npcData.js
@@ -0,0 +1,50 @@
+export const races = [
+  "Human",
+  "Elf",
+  "Dwarf",
+  "Halfling",
+  "Orc",
+  "Dragonborn",
+  "Tiefling",
+  "Half-Elf",
+  "Gnome",
+  "Half-Orc",
+];
+
+export const classes = [
+  "Fighter",
+  "Wizard",
+  "Rogue",
+  "Cleric",
+  "Barbarian",
+  "Paladin",
+  "Druid",
+  "Ranger",
+  "Sorcerer",
+  "Warlock",
+];
+
+export const alignments = [
+  "Lawful Good",
+  "Neutral Good",
+  "Chaotic Good",
+  "Lawful Neutral",
+  "True Neutral",
+  "Chaotic Neutral",
+  "Lawful Evil",
+  "Neutral Evil",
+  "Chaotic Evil",
+];
+
+export const backgrounds = [
+  "Acolyte",
+  "Criminal",
+  "Folk Hero",
+  "Noble",
+  "Sage",
+  "Soldier",
+  "Entertainer",
+  "Guild Artisan",
+  "Hermit",
+  "Outlander",
+];
