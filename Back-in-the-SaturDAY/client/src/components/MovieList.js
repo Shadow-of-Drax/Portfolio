121fbb05e12b9e2768f69d735ddf84a3b0e64bbc
James Warren <98415151+Shadow-of-Drax@users.noreply.github.com>
2024-09-26 15:24:15 -0400

    Update MovieList.js

--- Back-in-the-SaturDAY/client/src/components/MovieList.js
+++ Back-in-the-SaturDAY/client/src/components/MovieList.js
@@ -1,13 +1,30 @@
-import React from 'react';
 import '../styles/MovieList.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-const MovieList = ({ movies, onMovieClick }) => {
+const MovieList = () => {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        const fetchMovies = async () => {
+            try {
+                const response = await axios.get('http://localhost:5000/api/movies');
+                setMovies(response.data);
+            } catch (error) {
+                console.error('Error fetching movies:', error);
+            }
+        };
+
+        fetchMovies();
+    }, []);
+
     return (
         <div className="movie-list">
-            {movies.map((movie, index) => (
-                <div className="movie-item" key={index} onClick={() => onMovieClick(movie)}>
+            {movies.map(movie => (
+                <div key={movie._id} className="movie-item">
                     <img src={movie.poster} alt={movie.title} />
                     <h3>{movie.title}</h3>
+                    <a href={movie.videoUrl} target="_blank" rel="noopener noreferrer">Watch</a>
                 </div>
             ))}
         </div>
