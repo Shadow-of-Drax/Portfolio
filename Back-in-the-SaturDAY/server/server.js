382fb785bb428a8ccce1cd140f94620789f06987
James Warren <98415151+Shadow-of-Drax@users.noreply.github.com>
2024-09-26 14:53:56 -0400

    Update server.js

--- Back-in-the-SaturDAY/server/server.js
+++ Back-in-the-SaturDAY/server/server.js
@@ -1,13 +1,12 @@
-const express = require('express');
-const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const userRoutes = require('./routes/user');
 const movieRoutes = require('./routes/movie'); // Import movie routes
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
 const app = express();
 const PORT = process.env.PORT || 5000;
-require('dotenv').config();
 
 // Connect to MongoDB
 const uri = "mongodb+srv://jamesrwarren83:<L?zfS2q/wfFqZGN>@james-warren-database.29bff.mongodb.net/?retryWrites=true&w=majority&appName=James-Warren-Database"; // Make sure to set your actual password
@@ -19,25 +18,23 @@ const client = new MongoClient(uri, {
   }
 });
 
-async function run() {
-  try {
-    await client.connect();
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-
-    // Now you can set up your routes here to use MongoDB
-    // Example: Using client.db('yourDatabase').collection('yourCollection') for querying
-  } finally {
-    await client.close(); // This should be handled appropriately in your application
-  }
+async function connectToDatabase() {
+    try {
+        await client.connect();
+        console.log("Connected to MongoDB!");
+        app.use('/api', userRoutes); // Set up routes after connecting
+        app.use('/api/movies', movieRoutes);
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+    }
 }
 
-run().catch(console.dir);
-
+// Middleware
 app.use(cors());
 app.use(bodyParser.json());
-app.use('/api', userRoutes);
-app.use('/api/movies', movieRoutes);
+
+connectToDatabase();
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
