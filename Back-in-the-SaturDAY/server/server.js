10f162ce58e0061e7babb7176938f9c9d79f3d2a
James Warren <98415151+Shadow-of-Drax@users.noreply.github.com>
2024-09-26 14:05:46 -0400

    Update server.js

--- Back-in-the-SaturDAY/server/server.js
+++ Back-in-the-SaturDAY/server/server.js
@@ -4,15 +4,35 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const userRoutes = require('./routes/user');
 const movieRoutes = require('./routes/movie'); // Import movie routes
-require('dotenv').config();
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const PORT = process.env.PORT || 5000;
+require('dotenv').config();
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log("MongoDB connected"))
-    .catch(err => console.log(err));
+const uri = "mongodb+srv://jamesrwarren83:<L?zfS2q/wfFqZGN>@james-warren-database.29bff.mongodb.net/?retryWrites=true&w=majority&appName=James-Warren-Database"; // Make sure to set your actual password
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    // Now you can set up your routes here to use MongoDB
+    // Example: Using client.db('yourDatabase').collection('yourCollection') for querying
+  } finally {
+    await client.close(); // This should be handled appropriately in your application
+  }
+}
+
+run().catch(console.dir);
 
 app.use(cors());
 app.use(bodyParser.json());
