51344a77867bf77e6b66a21611330d7f61784728
James Warren <98415151+Shadow-of-Drax@users.noreply.github.com>
2024-09-26 11:24:18 -0400

    Setting up server

---
+++ Back-in-the-SaturDAY/server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
