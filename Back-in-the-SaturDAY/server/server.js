8af7e5d213ad4c613e90d25082b83d83b48c8478
James Warren <98415151+Shadow-of-Drax@users.noreply.github.com>
2024-09-26 12:30:28 -0400

    Update server.js

--- Back-in-the-SaturDAY/server/server.js
+++ Back-in-the-SaturDAY/server/server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const userRoutes = require('./routes/user');
 const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(bodyParser.json());
+app.use('/api', userRoutes);
 app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.get('/', (req, res) => {
