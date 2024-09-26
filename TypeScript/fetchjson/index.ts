d3bf8a6f31336c0ca0b2bdc0a13442333715b6a1
James Warren <98415151+Shadow-of-Drax@users.noreply.github.com>
2024-09-26 10:50:45 -0400

    Update index.ts

--- TypeScript/fetchjson/index.ts
+++ TypeScript/fetchjson/index.ts
@@ -24,7 +24,7 @@ axios.get(url).then(response => {
         logTodo (id, title, completed);
 });
 
-const logTodo = (id, title, completed) => {
+const logTodo = (id: number, title: string, completed: boolean) => {
     console.log(`
         The Todo with id: ${id}
         Has a title of: ${title}
