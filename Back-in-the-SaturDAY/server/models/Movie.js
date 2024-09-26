2985d1f520becbfc03f47730114f29c99f33bd8f
James Warren <98415151+Shadow-of-Drax@users.noreply.github.com>
2024-09-26 13:49:10 -0400

    Update Movie.js

--- Back-in-the-SaturDAY/server/models/Movie.js
+++ Back-in-the-SaturDAY/server/models/Movie.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const movieSchema = new mongoose.Schema({
     title: { type: String, required: true },
     poster: { type: String, required: true },
-    video: { type: String, required: true },
+    videoUrl: { type: String, required: true }, // Add videoUrl field for YouTube links
+    description: { type: String }, // Optional: Add a description field
 });
 
 const Movie = mongoose.model('Movie', movieSchema);
