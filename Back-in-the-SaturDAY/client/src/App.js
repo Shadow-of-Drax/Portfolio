a1920d6cf02a2aa6bf177c437636b3b5066014c2
James Warren <98415151+Shadow-of-Drax@users.noreply.github.com>
2024-09-26 12:10:33 -0400

    Update App.js

--- Back-in-the-SaturDAY/client/src/App.js
+++ Back-in-the-SaturDAY/client/src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import Navbar from './components/Navbar';
+import MovieList from './components/MovieList';
 import VideoPlayer from './components/VideoPlayer';
 import './styles/App.css';
 
 const App = () => {
-    const videoSrc = 'path_to_your_video.mp4'; // Update with your video path
+    const movies = [
+        { title: 'Movie 1', poster: 'path_to_poster1.jpg' },
+        { title: 'Movie 2', poster: 'path_to_poster2.jpg' },
+        { title: 'Movie 3', poster: 'path_to_poster3.jpg' },
+        { title: 'Movie 4', poster: 'path_to_poster4.jpg' },
+        // Add more movies here
+    ];
 
     return (
         <div className="app">
-            <h1>Back in the SaturDAY!</h1>
-            <VideoPlayer videoSrc={videoSrc} />
+            <Navbar />
+            <h2>Featured Movies</h2>
+            <MovieList movies={movies} />
+            {/* Add VideoPlayer if you want a featured video */}
+            {/* <VideoPlayer videoSrc={'path_to_your_video.mp4'} /> */}
         </div>
     );
 };
