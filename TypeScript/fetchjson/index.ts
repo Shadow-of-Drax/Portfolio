0fb9765637440884f6e856385161148915773b2f
James Warren <98415151+Shadow-of-Drax@users.noreply.github.com>
2024-09-26 10:34:52 -0400

    Update index.ts

--- TypeScript/fetchjson/index.ts
+++ TypeScript/fetchjson/index.ts
@@ -5,7 +5,13 @@ const url = 'https://jsonplaceholder.typicode.com/todos/1';
 axios.get(url).then(response => {
   const todo = response.data;
 
-    const ID = todo.id;
-    const title = todo.title;
+    const ID = todo.ID;
+    const title = todo.Title;
     const finished = todo.finished;
+
+    console.log(`
+        The Todo with ID: ${ID}
+        Has a title of: ${title}
+        Is it finished? ${finished}
+        `);
 });
