24219bf3f8682981c5689a306e1014fb3ac5677c
James Warren <98415151+Shadow-of-Drax@users.noreply.github.com>
2024-09-26 13:00:44 -0400

    updates

--- Back-in-the-SaturDAY/server/server.js
+++ Back-in-the-SaturDAY/server/server.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const userRoutes = require('./routes/user');
-const path = require('path');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("MongoDB connected"))
+    .catch(err => console.log(err));
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/api', userRoutes);
-app.use(express.static(path.join(__dirname, '../client/build')));
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-});
-
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
