9d95ef645685e3a7248fd99f8fec1b244da47340
James Warren <98415151+Shadow-of-Drax@users.noreply.github.com>
2024-09-26 12:08:07 -0400

    Update Navbar.js

---
+++ Back-in-the-SaturDAY/client/src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1>Back in the SaturDAY!</h1>
+            <div className="navbar-links">
+                <a href="#home">Home</a>
+                <a href="#movies">Movies</a>
+                <a href="#series">Series</a>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
