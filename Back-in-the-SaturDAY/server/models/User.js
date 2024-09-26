4235d3c36aca5b28ee9426c8a4a25d0f7c528cfb
James Warren <98415151+Shadow-of-Drax@users.noreply.github.com>
2024-09-26 13:12:41 -0400

    Update User.js

--- Back-in-the-SaturDAY/server/models/User.js
+++ Back-in-the-SaturDAY/server/models/User.js
@@ -4,6 +4,7 @@ const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     role: { type: String, default: 'user' },
+    email: { type: String, required: true, unique: true }, // Add email field
 });
 
 const User = mongoose.model('User', userSchema);
