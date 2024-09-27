43aa35c7dfdab4834aab4ba6f37ffc13a42f13ca
James Warren <98415151+Shadow-of-Drax@users.noreply.github.com>
2024-09-27 15:19:29 -0400

    Deleted

--- blog-site/blog-backend/src/routes/authRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { register, login } from '../controllers/authController';
-
-const router = Router();
-
-router.post('/register', register);
-router.post('/login', login);
-
-export default router;
