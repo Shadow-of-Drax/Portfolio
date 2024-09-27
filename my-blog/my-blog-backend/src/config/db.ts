c43d8d6248f597a0095a26d4dc9c6e4be75a6645
James Warren <98415151+Shadow-of-Drax@users.noreply.github.com>
2024-09-27 15:35:08 -0400

    working on backend

---
+++ my-blog/my-blog-backend/src/config/db.ts
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI as string, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected successfully.');
+  } catch (error) {
+    console.error('MongoDB connection failed:', error);
+    process.exit(1); // Exit the process with failure
+  }
+};
+
+export default connectDB;
